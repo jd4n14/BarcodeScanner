447f31e37b03f092d7aa9dd8c24611e48f62604e
g0d13 <g0d13@outlook.com>
2024-01-08 10:11:03 -0600

    add camera

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Diego Parra on 07/01/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    @State private var scannedCode: String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $scannedCode)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(scannedCode.isEmpty ? "Not yet scanned" : scannedCode)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
